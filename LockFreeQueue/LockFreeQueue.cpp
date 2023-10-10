327223089080ac5aec3ae98624b9c2ee33431a6f
KoutaibaShaar <skoutaiba@yahoo.com>
2023-10-10 14:34:25 +0100

    add serialisation for POD

--- LockFreeQueue/LockFreeQueue.cpp
+++ LockFreeQueue/LockFreeQueue.cpp
@@ -8,6 +8,7 @@
 #include <type_traits>
 #include <new>
 #include <memory>
+#include <vector>
 
 
 //#####################################################
@@ -252,8 +253,6 @@ private:
     unsigned int _writeCounter;
 };
 
-
-
 void function2()
 {
 
@@ -264,7 +263,7 @@ void function1()
 {
     char foo[8];
     //                            overwrite ebp     overwrite ret address
-    strncpy(foo, "12345678" "\x01\x02\x03\x04" "\x00\x4a\x84\x00", 16);
+   // strncpy(foo, "12345678" "\x01\x02\x03\x04" "\x00\x4a\x84\x00", 16);
 
 }
 
@@ -352,8 +351,95 @@ The msb has a special meaning (1, 0) in this notation allowing negatives and pos
 to negate a number in 2's complement (gor form negative to positive), we reverse the bits and 
 
 */
+
+
+template<typename T>
+struct message_header
+{
+    T id{};
+    uint32_t size = 0;
+};
+
+template<typename T>
+struct message
+{
+    message_header<T> header{};
+    std::vector<uint8_t> body;
+    
+    size_t size() const
+    {
+        return sizeof(message_header<T>) + body.size();
+    }
+    
+    friend std::ostream& operator << (std::ostream& os, const message<T>& msg)
+    {
+        os << "ID: " << int(msg.header.id) << " Size:" << msg.header.size;
+    }
+    
+    // pipe x and y into message
+    // msg << x << y
+    // push any POD type like data into the message buffer
+    // push a float DataType will be float etc
+    template<typename DataType>
+    friend message<T>& operator << (message<T>& msg, const DataType& data)
+    {
+        // check that data type being pushed is trivially copyable
+        static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to push");
+
+        // get current size
+        size_t i = msg.body.size();
+        
+        // resize vector to accomodate data being pushed in
+        msg.body.resize(i + sizeof(DataType));
+
+        // copy data into the newly allocated vector space - data() returns adddress of memory array used by the vector
+        // add i to move to address we want to copy to
+        std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+
+        msg.header.size = msg.size();
+
+        return msg;
+    }
+
+    template<typename DataType>
+    friend message<T>& operator >> (message<T>& msg, const DataType& data)
+    {
+        // check that data type being read is trivially copyable
+        static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to push");
+
+        // get size/position where the extraction of data starts
+        size_t i = msg.body.size() - sizeof(DataType);
+
+        // copy data from vector at position i to data 
+        // add i to move to address we want to copy to
+        std::memcpy(&data, msg.body.data() + i, &data, sizeof(DataType));
+
+        // making vector smaller has no pef overhead as no reallocation is involved
+        msg.body.resize(i);
+
+        return msg;
+    }
+};
+
+enum class CustomMsgTypes : uint32_t
+{
+    FireBulllet,
+    MovePlayer
+};
+
 int main()
 {
+    message<CustomMsgTypes> msg;
+    msg.header.id = CustomMsgTypes::FireBulllet;
+    int a = 1; bool b = true; float c = 3.14159f;
+    struct
+    {
+        float x;
+        float y;
+    } d[5];
+
+    msg << a << b << c << d;
+
     std::atomic<int*> ptr{ nullptr };
     ptr.store(new int{ 0 }, std::memory_order_release);
     std::cout << ptr.load() << "\n";
